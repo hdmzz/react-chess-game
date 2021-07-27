6d91202c64426f6781a5aa1608b574dd2ebda3d4
unknown <hdamitzian@gmail.com>
2021-07-27 15:41:45 +0200

    coloration ok pour le moment

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -8,29 +8,32 @@ export default function Chessboard() {
     let board = []
 
     verticalIndex.map(vIndex => {
-        horizontalIndex.map(hIndex => {
-            const position = [vIndex, hIndex]
-            const number = verticalIndex.indexOf(vIndex) + horizontalIndex.indexOf(hIndex) + 2 //on fait la somme des index des indexs vert et horizon pour determienr la couleur des cases
-            if (number % 2 === 0 ) {//l'operateur % renvoie le reste donc 1 si impair ou 0 si pair
-                board.push(
-                    <div key={position} className="boxWhite">
-                        {position}
-                    </div>
-                )
-            } else {
-                board.push(
-                    <div key={position} className="boxBlack">
-                        {position}
-                    </div>
-                )
-            }
-            // // la somme est pair alors couleur clair et inversement 
-             //comme la somme des premiers indexs ets egal à 0 alors il faut ajouter 2 à cette somme et verifier si la somme est divisible par 2 
-        })
+        return(
+            horizontalIndex.map(hIndex => {
+                const position = [vIndex, hIndex]
+                const number = verticalIndex.indexOf(vIndex) + horizontalIndex.indexOf(hIndex) + 2 //on fait la somme des index des indexs vert et horizon pour determiner la couleur des cases
+                if (number % 2 === 0 ) {//l'operateur % renvoie le reste donc 1 si impair ou 0 si pair
+                    return(board.push(
+                        <div key={position} className="boxWhite">
+                        </div>
+                    ))
+                } else {
+                    return(
+                        board.push(
+                        <div key={position} className="boxBlack">
+                        </div>
+                    ))
+                    
+                    
+                }
+                //la somme est pair alors couleur clair et inversement 
+                //comme la somme des premiers indexs ets egal à 0 alors il faut ajouter 2 à cette somme et verifier si la somme est divisible par 2 
+            })
+        )
     }) 
     return (
         <>
-            <div>
+            <div className="container">
                 {board && (
                     <div id="chessBoard">
                         {board}
