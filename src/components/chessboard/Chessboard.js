434c602d00e3cb5359a496701b1276ac73082a81
unknown <hdamitzian@gmail.com>
2021-07-28 10:19:22 +0200

    commit milieur gros changement pas encore de pieces

--- src/components/chessboard/Chessboard.js
+++ src/components/chessboard/Chessboard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Tile from '../tile/Tile'
 import './chessboard.css'
+const horizontalIndex = ["a", "b", "c", "d", "e", "f", "g", "h"]
+const verticalIndex = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 export default function Chessboard() {
-    const horizontalIndex = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    const verticalIndex = ["1", "2", "3", "4", "5", "6", "7", "8"]
     const pieces = []
     let board = []
 
@@ -21,22 +21,14 @@ export default function Chessboard() {
     for (let j = verticalIndex.length; j >= 0; j--){
         for (let i = 0; i < horizontalIndex.length; i++){
             const number = j + i + 2
-            if (number % 2 === 0){
-                board.push(
-                    <div className="boxWhite">
-                        
-                    </div>
-                )
-            } else {
-                board.push(
-                    <div className="boxBlack">
-                        
-                    </div>
-                )
-            }
+            board.push(<Tile number={number}/>)
         }
     }
     return (
-        <Tile/>
+        <div className="container">
+            <div id="chessboard">
+                {board}
+            </div>
+        </div>
     )
 }
--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import './tile.css'
 
-export default function Tile() {
-    return (
-        <div>
-            hello
-        </div>
-       /*  <div className="container">
-            {board && (
-                <div id="chessBoard">
-                    {board}
-                </div>
-            )}
-        </div> */
-    )
+export default function Tile({ number }) {
+    if (number % 2 === 0) {
+        return (
+            <div className="boxWhite">
+                     hello   
+            </div>
+        )
+    } else {
+        return (
+            <div className="boxBlack">
+                     hallo   
+            </div>
+        ) 
+    }
 }
