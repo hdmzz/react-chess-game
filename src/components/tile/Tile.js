a0508f53682daf5f26c4597df7aa6d7f8c90bcc2
unknown <hdamitzian@gmail.com>
2021-08-12 12:33:22 +0200

    positionement premiers pions

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -2,16 +2,24 @@ import React from 'react'
 import './tile.css'
 
 export default function Tile({ number, iIndex, jIndex }) {
+    let image = ""; 
+    if (jIndex === 6) {
+        image = "images/pawn_b.png"
+    }
+    if (jIndex === 1) {
+        image = "images/pawn_w.png"
+    }
+    
     if (number % 2 === 0) {
         return (
             <div className="boxWhite">
-                <img src="images/pawn_b.png"/>
+                <img src={image}/>
             </div>
         )
     } else {
         return (
             <div className="boxBlack">
-                {number}, {iIndex}, {jIndex}  
+                <img src={image}/>
             </div>
         ) 
     }
