42beda25e9e2bef1b0003b3b5eb1a31929574e56
unknown <hdamitzian@gmail.com>
2021-08-25 11:20:19 +0200

    deplacemnt pions noir debut

--- src/components/referee/referee.js
+++ src/components/referee/referee.js
@@ -2,12 +2,16 @@ import { teamTurn } from "../chessboard/Chessboard";
 
 export default class Referee {
     isValid(px, py, x, y, pieceType, teamType){
+        let turn = (teamType === teamTurn.WHITE) ? true : false;
         if (pieceType === 1){
-            if (teamType === teamTurn.WHITE ){//pas forcement necessaire
-                if (py === 1) {
+            if (py === 1 || py === 6){//pas forcement necessaire
+                if (turn){
                     if (y - py === 1 || y - py === 2){ //le pion ne peut pas reculer
-                        console.log('referee check le deplacement');
-                        console.log(px, py, x, y);
+                        return true
+                    }
+                }
+                if (!turn){
+                    if (py - y === 1 || py - y === 2){ //le pion ne peut pas reculer
                         return true
                     }
                 }
