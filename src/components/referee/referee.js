91436ac9c55bb424bb65eb2039ea0420bc8dea8e
Hugo Damitzian <hdamitzian@gmail.com>
2021-08-31 12:15:43 +0200

    commit tentative de cleaner le code

--- src/components/referee/referee.js
+++ src/components/referee/referee.js
@@ -4,53 +4,30 @@ export default class Referee {
     tileIsOccupied(x, y, chessboard) {
         const piece = chessboard.find( p => (p.x === x && p.y === y))
         if (piece) {
-            console.log(piece);
-            console.log('occupée');
             return true 
         } else {
-            console.log('libre');
             return false
         }
     }
 
     isValid(px, py, x, y, piece, team, chessboard){
         if (piece === pieceType.PAWN) {
-            if (team === true) {
-                if (py === 1){
-                    if (px === x && y - py === 1){ //le pion ne peut pas reculer
-                        if (!this.tileIsOccupied(x, y, chessboard)) {
-                            return true
-                        }
+            const row = (team === true) ? 1 : 6;//si team true ===> white alors axe x = a 1 sinon egal a 6
+            const pawnDirection = (team === true) ? 1 : -1;
+            if (py === row) {
+                if (px === x && y - py === 1*pawnDirection){
+                    if (!this.tileIsOccupied(x, y, chessboard)) {
+                        return true
                     }
-                    if (px === x && y - py === 2) {
-                        if (!this.tileIsOccupied(x, y, chessboard) && !this.tileIsOccupied(x, y-1, chessboard)){
-                            return true
-                        }
-                    }
-                } else {
-                    if (px === x && y - py === 1){
-                        if (!this.tileIsOccupied(x, y, chessboard)) {
-                            return true
-                        }
-                    } 
+                } else if (px === x && y - py === 2*pawnDirection) {
+                    if (!this.tileIsOccupied(x, y, chessboard) && !this.tileIsOccupied(x, y-pawnDirection, chessboard)){
+                        return true
+                    }   
                 }
-            } else if (team === false) {
-                if (py === 6) {
-                    if (px === x && py - y === 1) { //le pion ne peut pas reculer
-                        if (!this.tileIsOccupied(x, y, chessboard)) {
-                            return true
-                        }
-                    }
-                    if (px === x && py - y === 2) {
-                        if (!this.tileIsOccupied(x, y, chessboard) && !this.tileIsOccupied(x, y+1, chessboard)) {
-                            return true
-                        }
-                    }
-                } else {
-                    if (px === x && py - y === 1) {
-                        if (!this.tileIsOccupied(x, y, chessboard)) {
-                            return true
-                        }
+            } else {
+                if (px === x && y - py === pawnDirection) {
+                    if (!this.tileIsOccupied(x, y, chessboard)) {
+                        return true
                     }
                 }
             }
