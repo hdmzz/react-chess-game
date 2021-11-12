0057954e05d358a0b6aa86d69e40c50f3a8c5da1
unknown <hdamitzian@gmail.com>
2021-11-12 15:59:07 +0100

    position absolute

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -5,16 +5,14 @@ export default function Tile({number, image, x, y}) {
     if (number % 2 === 0) {
         return (
             <div className="box" style={{backgroundColor: "white"}} data-x={x} data-y={y}>
-                {x},{y}
-                <div  style={{position: 'absolute'}}></div> 
+                <div style={{position: 'absolute'}}>{x},{y}</div> 
                 {image && <div className="piece" style={{backgroundImage: `url(${image})`}}></div>} {/* si image non null alors on rend la div avec le background */}
             </div>
         )
     } else {
         return (
             <div className="box" style={{backgroundColor: "slateblue"}} data-x={x} data-y={y}>
-                {x},{y}
-                <div style={{position: 'absolute'}}></div>
+                <div style={{position: 'absolute'}}>{x},{y}</div>
                 {image && <div className="piece" style={{backgroundImage: `url(${image})`}}></div>}
             </div>
         ) 
